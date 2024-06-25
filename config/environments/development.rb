d6d522c74127f5cde4188b6a07774cd01b844943
Panchito <francisco.olivars.14@sansano.usm.cl>
2024-06-25 19:26:21 +0000

    feat: Update rubocop config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
 
   config.generators.after_generate do |files|
     parsable_files = files.filter { |file| file.end_with?('.rb') }
-    system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true) unless parsable_files.empty?
+    unless parsable_files.empty?
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
   end
 end
