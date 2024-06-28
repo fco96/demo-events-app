526acc42e4eceb8f6cb8fe43729831ec4b4df87a
Panchito <francisco.olivars.14@sansano.usm.cl>
2024-06-28 15:58:15 +0000

    feat: Add rspec & user tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
+
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    user.email = nil
+    expect(user).not_to be_valid
+  end
+
+  it 'is not valid without a first name' do
+    user.first_name = nil
+    expect(user).not_to be_valid
+  end
+
+  it 'is not valid without a last name' do
+    user.last_name = nil
+    expect(user).not_to be_valid
+  end
+
+  it 'returns the full name as a string' do
+    user.first_name = 'Pan'
+    user.last_name = 'Chito'
+    expect(user.full_name).to eq('Pan Chito')
+  end
+end
